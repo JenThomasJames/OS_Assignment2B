e04540dba7e46585c716e330ac9b5680d49c1df7
JenThomasJames <jenthomasjames123@gmail.com>
2023-03-06 21:59:34 +0530

    Feat: Add IO Queue Added an IOQueue for simulating the processes in waiting state

---
+++ IOQueue.java
@@ -0,0 +1,35 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class IOQueue {
+    private static IOQueue instance;
+    private Queue<Process> ioQueue;
+
+    private IOQueue() {
+        ioQueue = new PriorityQueue<>(Comparator.comparingInt(Process::getPriority).reversed());
+    }
+
+    public static IOQueue getInstance() {
+        if (instance == null) {
+            instance = new IOQueue();
+        }
+        return instance;
+    }
+
+    public void add(Process process) {
+        ioQueue.add(process);
+    }
+
+    public boolean remove(Process process) {
+        return ioQueue.remove(process);
+    }
+
+    public Process peek() {
+        return ioQueue.peek();
+    }
+
+    public boolean isEmpty() {
+        return ioQueue.isEmpty();
+    }
+}
